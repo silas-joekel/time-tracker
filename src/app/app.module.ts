003ccb79338a8664e1f55273c364c658747f9e13
Silas Jökel <silas@joekel.info>
2020-03-15 16:01:32 +0100

    feat: flex layout for app toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { APP_ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
     BrowserModule,
-    RouterModule.forRoot(APP_ROUTES),
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    RouterModule.forRoot(APP_ROUTES)
   ],
   declarations: [
     AppComponent
