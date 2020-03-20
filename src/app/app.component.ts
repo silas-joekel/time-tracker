0558a648348d14e70b7dc3d086ff93d62f0a506e
Silas Jökel <silas@joekel.info>
2020-03-20 22:55:31 +0100

    feat: navigation + minor refactorings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+interface Plugin {
+  identifier: string,
+  name: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
 export class AppComponent {
-  sidenavOpened = false;
+  plugins: Plugin[] = [
+    {
+      identifier: 'rocket-league',
+      name: 'Rocket League'
+    },
+    {
+      identifier: 'sleep',
+      name: 'Sleep'
+    }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -15,9 +16,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FlexLayoutModule,
+    MatButtonModule,
     MatCheckboxModule,
     MatIconModule,
+    MatListModule,
     MatSidenavModule,
     MatToolbarModule,
     RouterModule.forRoot(APP_ROUTES)
