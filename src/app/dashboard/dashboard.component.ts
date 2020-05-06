8808e6b77d33525943724d2221cf7e159f8d7d21
Silas Jökel <silas.joekel@ai4bd.org>
2020-05-06 18:40:04 +0200

    feat: call functions from emitted events

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -19,6 +19,18 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  startActivity(label: string): void {
+    this.activitiesService.startActivity(label);
+  }
+
+  stopActivity(id: string): void {
+    this.activitiesService.stopActivity(id);
+  }
+
+  deleteActivity(id: string): void {
+    this.activitiesService.deleteActivity(id);
+  }
+
   startNewActivity() {
     this.activitiesService.startActivity(this.newActivity);
     this.newActivity = '';
